5fbdb7a5efc111a303a82a8254714f063fd5bc03
BrunoSimbine <brunosimbine07012002@gmail.com>
2022-09-29 21:22:53 +0200

    second commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+var ctx = document.getElementById('canva').getContext('2d');
+
+// <canvas id="mixed-chart" width="800" height="450"></canvas>
+new Chart(document.getElementById('canva'), {
+  type: 'bar',
+  data: {
+    labels: ['1900', '1950', '1999', '2050', '2051', '2504', '2550', '2650'],
+    datasets: [
+      {
+        label: 'Contribuicao de Simpativantes',
+        type: 'line',
+        borderColor: '#1cc88a',
+        data: [808, 747, 975, 734, 833, 921, 783, 2478],
+        fill: false,
+      },
+      {
+        label: 'Contribuicao de Socios',
+        type: 'bar',
+        backgroundColor: '#36b9cc',
+        backgroundColorHover: '#3e95cd',
+        data: [133, 221, 783, 478, 133, 221, 383, 278],
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    legend: {
+        position: 'bottom'
+    },
+    title: {
+      display: false,
+      text: 'Contribuicao Mensal',
+    },
+  },
+});
